9ede458e8edd2b975ee0a0531e3ddeb93477a522
Itsuo Okamoto <itsuo.okamoto@recto.com>
2016-06-01 10:00:45 -0700

    checking in the original files for Feed Reader Testing

---
+++ P8_AddressBookSpec/src/AddressBook.js
@@ -0,0 +1,26 @@
+var AddressBook = function() {
+  this.contacts = [];
+  this.initialComplete = false;
+};
+
+AddressBook.prototype.getInitialContacts = function (cb) {
+  var self = this;
+  setTimeout(function() {
+    self.initialComplete = true;
+    if (cb) {
+      return cb();
+    }
+  }, 3);
+};
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+};
+
+AddressBook.prototype.getContact = function(index) {
+  return this.contacts[index];
+};
+
+AddressBook.prototype.deleteContact = function(index) {
+  return this.contacts.splice(index, 1);
+};
